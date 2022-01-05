68d156eac8a571974c31681892bf235f81746fdf
okeith12 <35121941+okeith12@users.noreply.github.com>
2022-01-04 19:39:31 -0600

    Updated Main and DBreader

--- main.go
+++ main.go
@@ -9,13 +9,14 @@ import (
 func main() {
 	dataBase := "first_db"
 	tableName := "Phone Numbers"
+	hasData := false
 	database,err :=dbReader.OpenDatabase(dataBase)
 	if err !=nil{
 		fmt.Println(err)
 	}
 	defer database.Close()
 
-	tableData := dbReader.ReadAllDataFromTable(database,&tableName,true)
+	tableData := dbReader.ReadAllDataFromTable(database,&tableName,hasData)
 	dbReader.UpdateData(tableData,&tableName,database)
 
 }
